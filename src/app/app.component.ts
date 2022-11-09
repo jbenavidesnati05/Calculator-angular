ee13f24a51f1d48b72add0961f2b9632b21a0a7e
Jorge Benavides Nati <jbenavides@sistecredito.com>
2022-11-09 17:19:05 -0500

    calculator finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'calculator';
+
+  title = 'Calculator';
+  numero1:number = 1;
+  numero2:number = 1;
+  Resultado:number = 0;
+
+  sumar():void{
+    this.Resultado = this.numero1 +this.numero2;
+  }
+  restar():void{
+    this.Resultado = this.numero1 -this.numero2;
+  }
+  multiplicar():void{
+    this.Resultado = this.numero1 *this.numero2;
+  }
+  dividir():void{
+    this.Resultado = this.numero1 /this.numero2;
+  }
+
 }
